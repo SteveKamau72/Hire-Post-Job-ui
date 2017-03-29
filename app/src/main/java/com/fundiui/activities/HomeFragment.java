cd9ffb78799d1ab85a800aaed6d8f544121ddae1
Steve Kamau <stevekamau72@gmail.com>
2017-03-29 22:28:57 +0300

    added post service ui

--- app/src/main/java/com/fundiui/activities/HomeFragment.java
+++ app/src/main/java/com/fundiui/activities/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.fundiui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,14 +15,29 @@ import com.fundiui.R;
  * @since 10/19/16
  */
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+//                post_layout
+        setUpViews(view);
+        return view;
+    }
+
+    private void setUpViews(View view) {
+        //post job activity
+        (view.findViewById(R.id.post_layout)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), PostJob.class));
+            }
+        });
     }
 }
